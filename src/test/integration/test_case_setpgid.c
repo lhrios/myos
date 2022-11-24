8fa4525dd8160b79497a5cb97e3f5826ab4a36eb
Luis Rios <lhrios@gmail.com>
2022-11-24 20:35:45 -0300

    Adjust project name

--- src/test/integration/test_case_setpgid.c
+++ src/test/integration/test_case_setpgid.c
@@ -1,14 +1,14 @@
 /*
  * Copyright 2022 Luis Henrique O. Rios
  *
- * This file is part of MyOS kernel.
+ * This file is part of MyOS.
  *
- * MyOS kernel is free software: you can redistribute it and/or modify
+ * MyOS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MyOS kernel is distributed in the hope that it will be useful,
+ * MyOS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
